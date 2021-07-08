3efb43e30c59ead74cd5d4a87033272097bd5654
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-08 11:23:40 -0300

    :wrench: config: Added config jest.config.js
    Config collectCoverageFrom and modulePaths.

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,17 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/*.stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/*.stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+  ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text-summary', 'lcov'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
   },
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
 };
