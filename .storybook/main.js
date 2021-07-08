cbbeef2474f2fbb5ef1f5865c04a14baf4fa4605
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-08 12:04:45 -0300

    :wrench: config: Config in ./.storybook/main.js
    Added addons "@storybook/addon-postcss"

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/components/**/*.stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss',
+  ],
 
   webpackFinal: async (config, { configType }) => {
     config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
